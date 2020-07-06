e033395341d8e2fbe26e04c533cfcc7cbd56dec1
Stepan Orlov <majorsteve@mail.ru>
2020-07-06 15:06:50 +0300

    system: Fixed cmake file.

--- src/system/FindSilverBulletsSystem.cmake
+++ src/system/FindSilverBulletsSystem.cmake
@@ -1,3 +1,3 @@
-find_library (SILVER_BULLETS_SYSTEM_LIBRARIES NAMES libsb_os.so sb_os.dll PATHS "${CMAKE_CURRENT_LIST_DIR}/../lib")
+find_library (SILVER_BULLETS_SYSTEM_LIBRARIES NAMES libsb_system.so sb_system.dll PATHS "${CMAKE_CURRENT_LIST_DIR}/../lib")
 find_path(SILVER_BULLETS_SYSTEM_INCLUDE_DIRS "silver_bullets/system/defs.h" PATHS "${CMAKE_CURRENT_LIST_DIR}/../include")
 
